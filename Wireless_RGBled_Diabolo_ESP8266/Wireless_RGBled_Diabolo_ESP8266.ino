493a25c1bb1ba9d340c1fcaf98a5c28d999cca19
darrydai <darry0703@gmail.com>
2016-06-15 16:43:19 +0800

    code update 20160615

--- Wireless_RGBled_Diabolo_ESP8266/Wireless_RGBled_Diabolo_ESP8266.ino
+++ Wireless_RGBled_Diabolo_ESP8266/Wireless_RGBled_Diabolo_ESP8266.ino
@@ -24,6 +24,7 @@ void setup()
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
+  pixels.begin(); 
   while (WiFi.status() != WL_CONNECTED) 
   {
     delay(500);
@@ -45,75 +46,81 @@ void loop()
   WiFiClient client = server.available();
   if (!client) 
   { 
-    Serial.print("Plz type the commend");
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  if (client) 
+  {
     while (client.connected())
     {
       if (client.available())
       {
         char c = client.read();
+        Serial.println(c);
         if(incoming && c == ' ')
         {
           incoming = 0;
         }
         if(c == '$')
         {
+          Serial.println("ready");
           incoming = 1; 
         }
         if(incoming == 1)
         {
-          Serial.println(c);
-            if(c=='1')
-            {
-              R=255;
-              G=255;
-              B=255;
-            }
-            if(c=='2')
-            {
-              R=0;
-              G=0;
-              B=0;
-            }
-            if(c=='a')
-            {
-              R=255;
-              G=90;
-              B=180;
-            }
-            if(c=='b')
-            {
-              R=255;
-              G=0;
-              B=255;
-            }
-            if(c=='c')
-            {
-              R=191;
-              G=62;
-              B=255;
-            }
-            if(c=='d')
-            {
-              R=0;
-              G=0;
-              B=255;
-            }
-            if(c=='e')
-            {
-              R=0;
-              G=160;
-              B=255;
-            }
-            for(int i=0;i<PixelNum;i++)
-            {
-              // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-              pixels.setPixelColor(i, pixels.Color(R,G,B)); // Moderately bright green color.
-              pixels.show(); // This sends the updated pixel color to the hardware.
-              delay(DelayTime); // Delay for a period of time (in milliseconds).
-            }
+          if(c=='1')
+          {
+            R=255;
+            G=255;
+            B=255;
+          }
+          if(c=='2')
+          {
+            R=0;
+            G=0;
+            B=0;
+          }
+          if(c=='a')
+          {
+            R=255;
+            G=90;
+            B=180;
+          }
+          if(c=='b')
+          {
+            R=255;
+            G=0;
+            B=255;
+          }
+          if(c=='c')
+          {
+            R=191;
+            G=62;
+            B=255;
+          }
+          if(c=='d')
+          {
+            R=0;
+            G=0;
+            B=255;
+          }
+          if(c=='e')
+          {
+            R=0;
+            G=160;
+            B=255;
+          }
+          for(int i=0;i<PixelNum;i++)
+          {
+            // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+            pixels.setPixelColor(i, pixels.Color(R,G,B)); // Moderately bright green color.
+            pixels.show(); // This sends the updated pixel color to the hardware.
+            delay(DelayTime); // Delay for a period of time (in milliseconds).
+          }
         }
-    //return; 
-      }   
+      }
     }
   }
 }
